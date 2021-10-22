b5dd822e8c7403a585ca3e1124abca55f6e48513
BASAK Semih <semihbasak25@gmail.com>
2021-10-22 15:49:20 +0200

    feature: user register + test

---
+++ src/Controller/User/RegisterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    protected EntityManagerInterface $entityManager;
+    protected UserPasswordHasherInterface $passwordHasher;
+    protected UserRepository $userRepository;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository
+    ) {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
+    /**
+     * @Route("/inscription", name="security_register")
+     */
+    public function registration(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search_email = $this->userRepository->findOneByEmail($user->getEmail());
+            if (!$search_email) {
+                $passwordHash = $this->passwordHasher->hashPassword($user, $user->getPassword());
+                $user->setPassword($passwordHash);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                $this->addFlash('success', 'Votre compte à bien été créee');
+                return $this->redirectToRoute('homePage');
+            }
+            $this->addFlash('warning', 'Cette adresse email est déjà utiliser');
+            $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+            return $this->render('user/register.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,25 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    protected UserPasswordHasherInterface $encoder;
+    public function __construct(UserPasswordHasherInterface $encoder)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('email@email.com')->setFirstName('Login')->setLastName('Test')->setPassword('password');
+        $password = $this->encoder->hashPassword($user, $user->getPassword());
+        $user->setPassword($password);
+        $manager->persist($user);
 
         $manager->flush();
     }
