6ff48bfc54e90aa2069f73668f3801cb5c68418b
BASAK Semih <semihbasak25@gmail.com>
2021-10-22 15:29:39 +0200

    feature: fix ci

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -10,9 +11,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homePage")
      */
-    public function home()
+    public function home(): Response
     {
         return $this->render('home/home.html.twig');
     }
-
 }
