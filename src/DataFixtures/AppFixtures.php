673382c73c79ae8702ca99db12030749d5b89192
BASAK Semih <semihbasak25@gmail.com>
2021-10-23 15:58:53 +0200

    feature: faker fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
@@ -21,28 +22,25 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('fr_FR');
         $user = new User();
         $user->setEmail('email@email.com')->setFirstName('Login')->setLastName('Test')->setPassword('password');
         $password = $this->encoder->hashPassword($user, $user->getPassword());
         $user->setPassword($password);
         $manager->persist($user);
 
-        $category_name = ['Canapés', 'Meubles TV', 'Chaises',
-            'Lits', 'Commodes', 'Mobilier de jardin', 'Meubles enfant', 'Meubles bébé'];
-        $article_name = ['Canapés modulables en tissu', 'Canapés 3 places en tissu',
-            "Canapés d'angle en tissu", 'Panneaux pour porte coulissante', 'Lits mezzanine et lits superposés'];
 
         for ($c = 0; $c < 5; $c++) {
             $category = new Category();
-            $category->setName($category_name[array_rand($category_name)]);
+            $category->setName($faker->name);
             $category->setSlug(strtolower($this->slugger->slug($category->getName())));
             $manager->persist($category);
             for ($a = 0; $a < 5; $a++) {
                 $article = new Article();
-                $article->setName($article_name[array_rand($article_name)]);
+                $article->setName($faker->name);
                 $article->setSlug(strtolower($this->slugger->slug($article->getName())));
                 $article->setCategory($category);
-                $article->setDescription("lorem lorem lorem");
+                $article->setDescription($faker->text);
                 $article->setQuantity(mt_rand(1, 20));
                 $article->setPrice(mt_rand(1000, 2000));
                 $manager->persist($article);
